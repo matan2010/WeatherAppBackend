31deb1b05308b245687ce8f16919df7807863560
Matan <matan203040@gmail.com>
2024-11-09 11:08:04 +0200

    add fun get_weather_data and connect to weather

--- weather_app_backend/settings.py
+++ weather_app_backend/settings.py
@@ -80,6 +80,15 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Redis default port and database 1
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
